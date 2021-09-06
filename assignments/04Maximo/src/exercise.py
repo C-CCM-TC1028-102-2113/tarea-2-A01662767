f72ec2c519d5f58ee7fa7b14a9f6ae50b48d0679
A01662767 <89958847+A01662767@users.noreply.github.com>
2021-09-06 13:38:02 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,7 +1,30 @@
 def main():
     #escribe tu código abajo de esta línea
+    print("NUMERO MAYOOR")
+    num1 = int(input("DAME EL NUMERO 1: "))
+    num2 = int(input("DAME EL NUMERO 2: "))
+    num3 = int(input("DAME EL NUMERO 3: "))
+
+
+    if num1 > num2:
+        print(num1) 
+        if num2 > num3:
+            print (num2)
+        else:
+             print(num3)
+                   
+    else:
+        print(num2)
+         
+        if num1 > num3:
+            print(num1)
+        else:
+            print(num3)
+            
+
     pass
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
+
